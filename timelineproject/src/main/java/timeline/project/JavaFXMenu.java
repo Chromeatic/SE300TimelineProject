5a931cff0f7578a01d1b8e8283127402f58b3d5d
Nathan Smith <177375451+Chromeatic@users.noreply.github.com>
2025-03-24 22:13:43 -0400

    Add files via upload

--- timelineproject/src/main/java/timeline/project/JavaFXMenu.java
+++ timelineproject/src/main/java/timeline/project/JavaFXMenu.java
@@ -1,6 +1,7 @@
 package timeline.project;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,18 +9,49 @@ import javafx.stage.Stage;
 
 public class JavaFXMenu extends Application {
 
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        JavaFXMenu.primaryStage = primaryStage;
         primaryStage.setTitle("JavaFX Menu");
 
-        // Load the FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainMenu.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root, 1920, 1080);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    @FXML
+    private void modeSelect() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/modeSelect.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1920, 1080);
+        primaryStage.setScene(scene);
+    }
+
+    @FXML
+    private void mainMenu() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainMenu.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1920, 1080);
+        primaryStage.setScene(scene);
+    }
+
+    @FXML
+    private void userStatistics() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/userStatistics.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1920, 1080);
+        primaryStage.setScene(scene);
+    }
+
+
 
     public static void main(String[] args) {
         launch(args);
---
+++ timelineproject/target/run.bat
@@ -0,0 +1,3 @@
+@echo off
+java --module-path "C:\javafx-sdk-23.0.2\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar timelineproject-1.0-SNAPSHOT.jar
+pause
